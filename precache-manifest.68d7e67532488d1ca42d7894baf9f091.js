366d9094cff7d87354eebc62c3f3043a9788a450
Carls13 <charlesshb98@gmail.com>
2018-12-30 14:13:54 -0400

    Updates

---
+++ precache-manifest.68d7e67532488d1ca42d7894baf9f091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "938a2e7c608b3a83335de29dec344d1b",
+    "url": "/star-wars/static/media/myAvatar.938a2e7c.svg"
+  },
+  {
+    "revision": "48e867e8c4414f7eb271",
+    "url": "/star-wars/static/js/runtime~main.48e867e8.js"
+  },
+  {
+    "revision": "2a8cfb86c1679e343712",
+    "url": "/star-wars/static/js/main.2a8cfb86.chunk.js"
+  },
+  {
+    "revision": "498922c6c88cb6704e82",
+    "url": "/star-wars/static/js/1.498922c6.chunk.js"
+  },
+  {
+    "revision": "2a8cfb86c1679e343712",
+    "url": "/star-wars/static/css/main.eadb2ffb.chunk.css"
+  },
+  {
+    "revision": "498922c6c88cb6704e82",
+    "url": "/star-wars/static/css/1.826fb8ea.chunk.css"
+  },
+  {
+    "revision": "f72bfd942b5417678f40324445383677",
+    "url": "/star-wars/index.html"
+  }
+];
