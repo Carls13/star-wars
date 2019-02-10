35cd26c837bda9e4524181947f3e95746963a160
Carls13 <charlesshb98@gmail.com>
2019-02-09 20:49:19 -0400

    Updates

---
+++ precache-manifest.a704e22077a0db61a393cedf1e04b0a2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b9c4936b60edabb612fbd1a95856c5",
+    "url": "/star-wars/static/media/SWLogo.21b9c493.png"
+  },
+  {
+    "revision": "cd414b26c14d273b9840",
+    "url": "/star-wars/static/css/main.811fe824.chunk.css"
+  },
+  {
+    "revision": "3e8e3008b11375ff5aef20c914fd8623",
+    "url": "/star-wars/static/media/planets.3e8e3008.png"
+  },
+  {
+    "revision": "5cd02389546e08db44ef",
+    "url": "/star-wars/static/js/1.5cd02389.chunk.js"
+  },
+  {
+    "revision": "48e867e8c4414f7eb271",
+    "url": "/star-wars/static/js/runtime~main.48e867e8.js"
+  },
+  {
+    "revision": "cd414b26c14d273b9840",
+    "url": "/star-wars/static/js/main.cd414b26.chunk.js"
+  },
+  {
+    "revision": "3f6d2bbb987d597af76e366fadcb0b28",
+    "url": "/star-wars/static/media/myself.3f6d2bbb.jpg"
+  },
+  {
+    "revision": "f2b2136af21e85f2318f92ac685ea8e5",
+    "url": "/star-wars/static/media/loader.f2b2136a.svg"
+  },
+  {
+    "revision": "ac3fa3db0aa1785ff53bdfc1dafb0121",
+    "url": "/star-wars/static/media/characters.ac3fa3db.png"
+  },
+  {
+    "revision": "5cd02389546e08db44ef",
+    "url": "/star-wars/static/css/1.a34f4677.chunk.css"
+  },
+  {
+    "revision": "7c84d2a9c377a96b97e8f582008b7fcf",
+    "url": "/star-wars/index.html"
+  }
+];
